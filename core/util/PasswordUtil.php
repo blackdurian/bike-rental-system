e23fa1a06829ac6bbb8b6b71ffc22c79cd755719
lwqec <33524888+blackdurian@users.noreply.github.com>
2020-09-11 04:38:32 +0800

    update passwordUtil

--- core/util/PasswordUtil.php
+++ core/util/PasswordUtil.php
@@ -1,6 +1,16 @@
 <?php
+
 // to hash password
+function hashPassword($myPassword){ // string
+return password_hash($myPassword, PASSWORD_DEFAULT); //string 
+}
+
+
 
 // encode password
+function verifyPassword($myPassword, $hashed_password){
+return password_verify($myPassword, $hashed_password);
+}
 
+ 
 ?>
